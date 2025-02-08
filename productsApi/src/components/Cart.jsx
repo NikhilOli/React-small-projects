ab5ae2d61f477e5d414d490c466063f2e37bc64d
NIkhil Oli <162276432+NikhilOli@users.noreply.github.com>
2025-02-08 21:19:56 +0545

    Update Cart.jsx

--- productsApi/src/components/Cart.jsx
+++ productsApi/src/components/Cart.jsx
@@ -15,7 +15,7 @@ const Cart = () => {
     )
     return (
         <>
-        <Toaster position="top-right" richColors />
+        <Toaster position="bottom-right" richColors />
         <div className="container mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
             
             {
